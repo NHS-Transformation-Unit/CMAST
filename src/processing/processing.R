7060863c66bea5714ac5bc4c6a9dee7d7491415b
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2024-02-02 11:00:43 +0000

    Creation of Trust data sets by Month, by day of week and by time of day, and creation of site data sets by Month, by day of week and by time of day, commit to processing.r

--- src/processing/processing.R
+++ src/processing/processing.R
@@ -1,6 +1,9 @@
+
+# Trust level metrics -----------------------------------------------------
+
 # Trust time series
 
-Trust_Attendances <- ECDS_MH_attendances %>%
+Trust_Attendances_Monthly <- ECDS_MH_attendances %>%
   select(Der_EC_Arrival_Date_Time,
          Provider_Name,
          Der_Provider_Code,
@@ -9,15 +12,46 @@ Trust_Attendances <- ECDS_MH_attendances %>%
          "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
          "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
   group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
-           day_of_week,
-           hour_of_day,
            Provider_Name,
            Der_Provider_Code) %>%
   summarise(Total_attendances = sum(MH_Flag))
 
+# Trust day of week
+
+Trust_Attendances_Weekly <- ECDS_MH_attendances %>%
+  select(Der_EC_Arrival_Date_Time,
+         Provider_Name,
+         Der_Provider_Code,
+         MH_Flag) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(day_of_week,
+           Provider_Name,
+           Der_Provider_Code) %>%
+  summarise(Total_attendances = sum(MH_Flag))
+
+# Trust hour of day
+
+Trust_Attendances_hourly <- ECDS_MH_attendances %>%
+  select(Der_EC_Arrival_Date_Time,
+         Provider_Name,
+         Der_Provider_Code,
+         MH_Flag) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(hour_of_day,
+           Provider_Name,
+           Der_Provider_Code) %>%
+  summarise(Total_attendances = sum(MH_Flag))
+
+
+# Site level metrics ------------------------------------------------------
+
 # Site time series
 
-Site_Attendances <- ECDS_MH_attendances %>%
+Site_Attendances_Monthly <- ECDS_MH_attendances %>%
   select(Der_EC_Arrival_Date_Time,
          Provider_Site,
          Der_Provider_Site_Code,
@@ -27,8 +61,40 @@ Site_Attendances <- ECDS_MH_attendances %>%
          "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
          "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
   group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
-           day_of_week,
-           hour_of_day,
+           Provider_Site,
+           Der_Provider_Site_Code,
+           EC_Department_Type) %>%
+  summarise(Total_attendances = sum(MH_Flag))
+
+# Site day of week
+
+Site_Attendances_Weekly <- ECDS_MH_attendances %>%
+  select(Der_EC_Arrival_Date_Time,
+         Provider_Site,
+         Der_Provider_Site_Code,
+         EC_Department_Type,
+         MH_Flag) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(day_of_week,
+           Provider_Site,
+           Der_Provider_Site_Code,
+           EC_Department_Type) %>%
+  summarise(Total_attendances = sum(MH_Flag))
+
+# Site hour of day
+
+Site_Attendances_Weekly <- ECDS_MH_attendances %>%
+  select(Der_EC_Arrival_Date_Time,
+         Provider_Site,
+         Der_Provider_Site_Code,
+         EC_Department_Type,
+         MH_Flag) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(hour_of_day,
            Provider_Site,
            Der_Provider_Site_Code,
            EC_Department_Type) %>%
