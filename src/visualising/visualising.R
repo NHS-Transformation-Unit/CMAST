863120c52cfd1a189cd82e1e0462758372496f2f
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2024-02-02 11:01:43 +0000

    Creation of small multiples of Trust monthly, day of week and hour of day charts, and creation of site monthly chart commit to visualising.r

--- src/visualising/visualising.R
+++ src/visualising/visualising.R
@@ -1,18 +1,9 @@
-# Visualising Sites
 
-ggplot(Site_Attendances, aes(x = Month, y = Total_attendances, fill = Provider_Site, group = Provider_Site)) +
-  geom_line(stat = "identity") + 
-  labs(title = "Mental Health attendances by site",
-       caption = "Source: ECDS",
-       x = "Month",
-       y = "Total Attendances") +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1),
-        legend.position = "bottom") +
-  scale_fill_viridis_d()
+# Trust level visuals -----------------------------------------------------
 
 # Visualising Trusts in one line chart
 
-ggplot(Trust_Attendances, aes(x = Month, y = Total_attendances, fill = Der_Provider_Code, group = Provider_Name)) +
+ggplot(Trust_Attendances_Monthly, aes(x = Month, y = Total_attendances, colour = Der_Provider_Code, group = Provider_Name)) +
   geom_line(stat = "identity") + 
   labs(title = "Mental Health attendances by trust",
        caption = "Source: ECDS",
@@ -23,17 +14,56 @@ ggplot(Trust_Attendances, aes(x = Month, y = Total_attendances, fill = Der_Provi
 
 # Create small multiples of line charts
 
-ggplot(Trust_Attendances, aes(x = Month, y = Total_attendances, colour = Der_Provider_Code, group = Provider_Name)) +
+ggplot(Trust_Attendances_Monthly, aes(x = Month, y = Total_attendances, colour = Der_Provider_Code, group = Provider_Name)) +
   geom_line(stat = "identity") +
   facet_wrap(~Provider_Name, scales = "free_x") +
+  labs(title = "Mental Health attendances by trust",
+       caption = "Source: ECDS",
+       x = "Month",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance
+
+ggplot(Trust_Attendances_Weekly, aes(x = day_of_week, y = Total_attendances, fill = Der_Provider_Code, group = Provider_Name)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~Provider_Name, scales = "free_x") +
+  labs(title = "Mental Health attendances by trust",
+       caption = "Source: ECDS",
+       x = "Day of the Week",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance
+
+ggplot(Trust_Attendances_hourly, aes(x = hour_of_day, y = Total_attendances, fill = Der_Provider_Code, group = Provider_Name)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~Provider_Name, scales = "free_x") +
+  labs(title = "Mental Health attendances by trust",
+       caption = "Source: ECDS",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+
+# Site level visuals ------------------------------------------------------
+
+# Create small multiples of line charts
+
+ggplot(Site_Attendances_Monthly, aes(x = Month, y = Total_attendances, colour = EC_Department_Type, group = Provider_Site)) +
+  geom_line(stat = "identity") +
+  facet_wrap(~Provider_Site, scales = "free_x") +
+  labs(title = "Mental Health attendances by site",
+       caption = "Source: ECDS",
+       x = "Month",
+       y = "Total Attendances") +
   theme(axis.text.x = element_text(angle = 60, hjust = 1),
         legend.position = "bottom",
-        plot.background = element_rect(fill = "white")) +
-  scale_colour_manual(values = c('RBS' = "blue",
-                                 'RJN' = "red",
-                                 'RWW' = "pink",
-                                 'RBT' = "green",
-                                 'RJR' = "purple",
-                                 'RBL' = "orange",
-                                 'REM' = "cyan",
-                                 'RBN' = "magenta"))
+        plot.background = element_rect(fill = "white"))
+
