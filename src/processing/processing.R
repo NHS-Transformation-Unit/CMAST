990e09ae1e8b3200319544f1eda6679a72a35a3e
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2024-02-02 10:17:51 +0000

    Create site level and trust level data frames commit to processing.r

--- src/processing/processing.R
+++ src/processing/processing.R
@@ -1,19 +1,35 @@
-# Create Trust tables
+# Trust time series
 
 Trust_Attendances <- ECDS_MH_attendances %>%
   select(Der_EC_Arrival_Date_Time,
          Provider_Name,
+         Der_Provider_Code,
          MH_Flag) %>%
-  mutate(Der_EC_Arrival_Date_Time = as.Date(Der_EC_Arrival_Date_Time)) %>%
-  group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),Provider_Name) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
+           day_of_week,
+           hour_of_day,
+           Provider_Name,
+           Der_Provider_Code) %>%
   summarise(Total_attendances = sum(MH_Flag))
 
-# Create Site tables
+# Site time series
 
 Site_Attendances <- ECDS_MH_attendances %>%
   select(Der_EC_Arrival_Date_Time,
          Provider_Site,
+         Der_Provider_Site_Code,
+         EC_Department_Type,
          MH_Flag) %>%
-  mutate(Der_EC_Arrival_Date_Time = as.Date(Der_EC_Arrival_Date_Time)) %>%
-  group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),Provider_Site) %>%
+  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time)) %>%
+  group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
+           day_of_week,
+           hour_of_day,
+           Provider_Site,
+           Der_Provider_Site_Code,
+           EC_Department_Type) %>%
   summarise(Total_attendances = sum(MH_Flag))
