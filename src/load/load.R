45ffad192959b0a11146301480f0867fec60ec9a
ASW-Analyst <andy.wilson8@nhs.net>
2024-02-05 22:15:18 +0000

    Absolute path

--- src/load/load.R
+++ src/load/load.R
@@ -1,3 +1,3 @@
 # Load in the ECDS data extracted using SQL
 
-ECDS_MH_attendances <- read.csv("data/ECDS_MH_Attendance.csv")
+ECDS_MH_attendances <- read.csv(paste0(here(),"/data/ECDS_MH_Attendance.csv"))
