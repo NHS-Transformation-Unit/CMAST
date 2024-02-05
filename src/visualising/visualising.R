769027826d23501ec8da24632d8fc98fec22e9ea
Wickham Simon (MLCSU) <Simon.Wickham@mlcsu.nhs.uk>
2024-02-05 12:34:55 +0000

    Creation of financial year based weekly attendances commit to visualising.R

--- src/visualising/visualising.R
+++ src/visualising/visualising.R
@@ -3,8 +3,19 @@
 
 # Visualising attendance time series
 
-ggplot(System_Attendances_Monthly, aes(x = Month, y = Total_attendances)) +
+ggplot(System_Attendances_Monthly, aes(x = Month, y = Total_attendances, colour = "red")) +
   geom_line(stat = "identity") + 
+  labs(title = "Mental Health attendances",
+       caption = "Source: SUS Emergency Care data Apr-19 to Dec-23",
+       x = "Month",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "bottom")
+
+# Visualising attendance by in hours
+
+ggplot(System_Attendances_Monthly_Hours, aes(x = Month, y = Total_attendances, fill = In_hours)) +
+  geom_bar(stat = "identity") + 
   labs(title = "Mental Health attendances",
        caption = "Source: SUS Emergency Care data Apr-19 to Dec-23",
        x = "Month",
@@ -19,6 +30,71 @@ ggplot(System_Attendances_Weekly, aes(x = hour_of_day, y = Total_attendances, fi
   facet_wrap(~day_of_week, scales = "free_x") +
   labs(title = "Mental Health attendances by day",
        caption = "Source: SUS Emergency Care data Apr-19 to Dec-23",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance by hour (19-20)
+
+ggplot(System_Attendances_Weekly_19, aes(x = hour_of_day, y = Total_attendances, fill = day_of_week, group = day_of_week)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~day_of_week, scales = "free_x") +
+  labs(title = "Mental Health attendances by day for the Financial Year 2019-20",
+       caption = "Source: SUS Emergency Care data Apr-19 to Mar-20",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance by hour (20-21)
+
+ggplot(System_Attendances_Weekly_20, aes(x = hour_of_day, y = Total_attendances, fill = day_of_week, group = day_of_week)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~day_of_week, scales = "free_x") +
+  labs(title = "Mental Health attendances by day for the Financial Year 2020-21",
+       caption = "Source: SUS Emergency Care data Apr-20 to Mar-21",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance by hour (21-22)
+
+ggplot(System_Attendances_Weekly_21, aes(x = hour_of_day, y = Total_attendances, fill = day_of_week, group = day_of_week)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~day_of_week, scales = "free_x") +
+  labs(title = "Mental Health attendances by day for the Financial Year 2021-22",
+       caption = "Source: SUS Emergency Care data Apr-21 to Mar-22",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance by hour (22-23)
+
+ggplot(System_Attendances_Weekly_22, aes(x = hour_of_day, y = Total_attendances, fill = day_of_week, group = day_of_week)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~day_of_week, scales = "free_x") +
+  labs(title = "Mental Health attendances by day for the Financial Year 2022-23",
+       caption = "Source: SUS Emergency Care data Apr-22 to Mar-23",
+       x = "Hour of the day",
+       y = "Total Attendances") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "none",
+        plot.background = element_rect(fill = "white"))
+
+# Weekly distribution of attendance by hour (23-24)
+
+ggplot(System_Attendances_Weekly_23, aes(x = hour_of_day, y = Total_attendances, fill = day_of_week, group = day_of_week)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~day_of_week, scales = "free_x") +
+  labs(title = "Mental Health attendances by day for the Financial Year 2023-24",
+       caption = "Source: SUS Emergency Care data Apr-23 to Dec-23",
        x = "Hour of the day",
        y = "Total Attendances") +
   theme(axis.text.x = element_text(angle = 60, hjust = 1),
