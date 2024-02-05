8544faa6f09351f6db1f16ade216d122b21cf70e
ASW-Analyst <andy.wilson8@nhs.net>
2024-02-05 22:15:46 +0000

    Move processing steps to cleaning, add waiting time quantile calculations

--- src/processing/processing.R
+++ src/processing/processing.R
@@ -3,7 +3,16 @@
 
 ECDS_MH_attendances_clean <- ECDS_MH_attendances %>%
   mutate(Der_EC_Arrival_Date_Time = as.POSIXct(Der_EC_Arrival_Date_Time, format = "%d/%m/%Y %H:%M"),
-         Month = as.Date(trunc(Der_EC_Arrival_Date_Time, "month")))
+         Month = as.Date(trunc(Der_EC_Arrival_Date_Time, "month")),
+         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
+         "hour_of_day" = hour(Der_EC_Arrival_Date_Time),
+         "In_hours" = ifelse(hour_of_day < 8 | hour_of_day >= 18, "Out of hours", "In hours"),
+         "FYear" = case_when(Month >= as.Date("2019-04-01") & Month <= as.Date("2020-03-31") ~ "2019/20",
+                             Month >= as.Date("2020-04-01") & Month <= as.Date("2021-03-31") ~ "2020/21",
+                             Month >= as.Date("2021-04-01") & Month <= as.Date("2022-03-31") ~ "2021/22",
+                             Month >= as.Date("2022-04-01") & Month <= as.Date("2023-03-31") ~ "2022/23",
+                             Month >= as.Date("2023-04-01") & Month <= as.Date("2024-03-31") ~ "2023/24")) %>%
+  mutate(day_of_week = factor(day_of_week, levels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")))
 
 
 # Filter Parameters -------------------------------------------------------
@@ -21,39 +30,56 @@ ED_23_24 <- ymd("2024-04-01") # end date for FY 2023-24
 
 # System level metrics ----------------------------------------------------
 
-System_Attendances <- ECDS_MH_attendances_clean %>%
-  select(Der_EC_Arrival_Date_Time,
-         Month,
-         EC_Departure_Time_Since_Arrival,
-         MH_Flag) %>%
-  mutate("day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
-         "hour_of_day" = hour(Der_EC_Arrival_Date_Time),
-         "In_hours" = ifelse(hour_of_day < 8 | hour_of_day >= 18, "Out of hours", "In hours"))
 
 # System monthly attendances
 
-System_Attendances_Monthly <- System_Attendances %>%
+System_Attendances_Monthly <- ECDS_MH_attendances_clean %>%
   group_by(Month) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
+
+# Combined attendances week and hour
+
+System_Attendances_day_hour <- ECDS_MH_attendances_clean %>%
+  group_by(day_of_week, hour_of_day) %>%
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # System monthly attendances in vs out
 
-System_Attendances_Monthly_Hours <- System_Attendances %>%
-  group_by(Month = floor_date(Der_EC_Arrival_Date_Time, "month"),
-           In_hours) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+System_Attendances_Monthly_Hours <- ECDS_MH_attendances_clean %>%
+  group_by(Month, In_hours) %>%
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # System day of week
 
-System_Attendances_Weekly <- System_Attendances %>%
+System_Attendances_Weekly <- ECDS_MH_attendances_clean %>%
   group_by(day_of_week,
            hour_of_day,
            In_hours) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # System day of week (Apr19 to Mar20)
 
-System_Attendances_Weekly_19 <- System_Attendances %>%
+System_Attendances_Weekly_19 <- ECDS_MH_attendances_clean %>%
   filter(Der_EC_Arrival_Date_Time >= SD_19_20 & Der_EC_Arrival_Date_Time < ED_19_20) %>%
   group_by(day_of_week,
            hour_of_day,
@@ -62,7 +88,7 @@ System_Attendances_Weekly_19 <- System_Attendances %>%
 
 # System day of week (Apr20 to Mar21)
 
-System_Attendances_Weekly_20 <- System_Attendances %>%
+System_Attendances_Weekly_20 <- ECDS_MH_attendances_clean %>%
   filter(Der_EC_Arrival_Date_Time >= SD_20_21 & Der_EC_Arrival_Date_Time < ED_20_21) %>%
   group_by(day_of_week,
            hour_of_day,
@@ -71,7 +97,7 @@ System_Attendances_Weekly_20 <- System_Attendances %>%
 
 # System day of week (Apr21 to Mar22)
 
-System_Attendances_Weekly_21 <- System_Attendances %>%
+System_Attendances_Weekly_21 <- ECDS_MH_attendances_clean %>%
   filter(Der_EC_Arrival_Date_Time >= SD_21_22 & Der_EC_Arrival_Date_Time < ED_21_22) %>%
   group_by(day_of_week,
            hour_of_day,
@@ -80,7 +106,7 @@ System_Attendances_Weekly_21 <- System_Attendances %>%
 
 # System day of week (Apr22 to Mar23)
 
-System_Attendances_Weekly_22 <- System_Attendances %>%
+System_Attendances_Weekly_22 <- ECDS_MH_attendances_clean %>%
   filter(Der_EC_Arrival_Date_Time >= SD_22_23 & Der_EC_Arrival_Date_Time < ED_22_23) %>%
   group_by(day_of_week,
            hour_of_day,
@@ -89,7 +115,7 @@ System_Attendances_Weekly_22 <- System_Attendances %>%
 
 # System day of week (Apr23 to Mar24)
 
-System_Attendances_Weekly_23 <- System_Attendances %>%
+System_Attendances_Weekly_23 <- ECDS_MH_attendances_clean %>%
   filter(Der_EC_Arrival_Date_Time >= SD_23_24 & Der_EC_Arrival_Date_Time < ED_23_24) %>%
   group_by(day_of_week,
            hour_of_day,
@@ -98,80 +124,88 @@ System_Attendances_Weekly_23 <- System_Attendances %>%
 
 # Trust level metrics -----------------------------------------------------
 
-Trust_Attendances <- ECDS_MH_attendances %>%
-  select(Der_EC_Arrival_Date_Time,
-         Provider_Name,
-         Der_Provider_Code,
-         EC_Conclusion_Time_Since_Arrival,
-         MH_Flag) %>%
-  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
-         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
-         "hour_of_day" = hour(Der_EC_Arrival_Date_Time),
-         "In_hours" = ifelse(hour_of_day < 8 | hour_of_day > 18, "Out of hours", "In hours"))
-
 # Trust monthly attendances
 
-Trust_Attendances_Monthly <- Trust_Attendances %>%
-  group_by(Month = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
+Trust_Attendances_Monthly <- ECDS_MH_attendances_clean %>%
+  group_by(Month,
            Provider_Name,
            Der_Provider_Code) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # Trust day of week
 
-Trust_Attendances_Weekly <- Trust_Attendances %>%
+Trust_Attendances_Weekly <- ECDS_MH_attendances_clean %>%
   group_by(day_of_week,
            Provider_Name,
            Der_Provider_Code,
            In_hours) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # Trust hour of day
 
-Trust_Attendances_Hourly <- Trust_Attendances %>%
+Trust_Attendances_Hourly <- ECDS_MH_attendances_clean %>%
     group_by(hour_of_day,
            Provider_Name,
            Der_Provider_Code) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # Site level metrics ------------------------------------------------------
 
-Site_Attendances <- ECDS_MH_attendances %>%
-  select(Der_EC_Arrival_Date_Time,
-         Provider_Site,
-         Der_Provider_Site_Code,
-         EC_Department_Type,
-         EC_Conclusion_Time_Since_Arrival,
-         MH_Flag) %>%
-  mutate(Der_EC_Arrival_Date_Time = ymd_hms(Der_EC_Arrival_Date_Time),
-         "day_of_week" = wday(Der_EC_Arrival_Date_Time, label = TRUE),
-         "hour_of_day" = hour(Der_EC_Arrival_Date_Time),
-         "In_hours" = ifelse(hour_of_day < 8 | hour_of_day > 18, "Out of hours", "In hours"))
 
 # Site monthly attendances
 
-Site_Attendances_Monthly <- Site_Attendances %>%
-  group_by(Month_1 = format(Der_EC_Arrival_Date_Time, "%Y-%m"),
+Site_Attendances_Monthly <- ECDS_MH_attendances_clean %>%
+  group_by(Month,
            Provider_Site,
            Der_Provider_Site_Code,
            EC_Department_Type) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # Site day of week
 
-Site_Attendances_Weekly <- Site_Attendances %>%
+Site_Attendances_Weekly <- ECDS_MH_attendances_clean %>%
   group_by(day_of_week,
            Provider_Site,
            Der_Provider_Site_Code,
            EC_Department_Type,
            In_hours) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
 
 # Site hour of day
 
-Site_Attendances_Hourly <- Site_Attendances %>%
+Site_Attendances_Hourly <- ECDS_MH_attendances_clean %>%
   group_by(hour_of_day,
            Provider_Site,
            Der_Provider_Site_Code,
            EC_Department_Type) %>%
-  summarise(Total_attendances = sum(MH_Flag))
+  summarise(Total_attendances = sum(MH_Flag),
+            P10_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.10),
+            P25_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.25),
+            P50_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.50),
+            P75_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.75),
+            P90_wait_time = quantile(EC_Departure_Time_Since_Arrival, 0.90))
